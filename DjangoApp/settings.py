896249e540fb7a285cdee5daaa895db7dd860257
alphaoumardiallo <alphaoumardev@outlook.com>
2023-04-07 21:58:02 +0800

    the

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-ah2xj@9c$f_$86a-c!_*!f4ls)_q6)^f&c)_qj@#qc@v*ag5co
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['170.187.220.180']
+ALLOWED_HOSTS = ['170.187.220.180']
 
 # Application definition
 
